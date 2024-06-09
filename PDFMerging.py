ffd09730ded7d8516bae21163f0429761489633d
SarvaniAnkala <134192953+SarvaniAnkala@users.noreply.github.com>
2024-06-10 02:50:38 +0530

    Update PDFMerging.py

--- PDFMerging.py
+++ PDFMerging.py
@@ -1,14 +1,19 @@
 import PyPDF2
-import sys
 import os
+import sys
 
+# Create an instance of PdfMerger
 merger = PyPDF2.PdfMerger()
 
+# Loop through all files in the current directory
 for file in os.listdir(os.curdir):
+    # Check if the file is a PDF
     if file.endswith(".pdf"):
+        # Append the PDF file to the merger
         merger.append(file)
 
+# Write the combined PDF to a new file
 merger.write("combinedBSUniDocs.pdf")
 
+# Close the merger
 merger.close()
-
